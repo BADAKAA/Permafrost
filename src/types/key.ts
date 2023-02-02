3f525e3104e907168301a01be3c676c12baf83ed
BADAKAA <Bastissh@gmail.com>
2023-02-02 15:32:44 +0100

    spacing

--- src/types/key.ts
+++ src/types/key.ts
@@ -1,19 +1,19 @@
-const keyConversions:{[key:string]:Key} = {
-    w:"ArrowUp",
-    s:"ArrowDown",
-    a:"ArrowLeft",
-    d:"ArrowRight",
-    NumpadEnter:"Enter",
+const keyConversions: { [key: string]: Key } = {
+    w: "ArrowUp",
+    s: "ArrowDown",
+    a: "ArrowLeft",
+    d: "ArrowRight",
+    NumpadEnter: "Enter",
 }
 
 const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Enter", "Space", "Escape"];
 export type Key = typeof keys[number];
 
-const isValidKey= (key:string): key is Key => keys.indexOf(key)!=-1;
+const isValidKey = (key: string): key is Key => keys.indexOf(key) != -1;
 
-const convertKey = (key:string) => key in keyConversions ? keyConversions[key] : key;
+const convertKey = (key: string) => key in keyConversions ? keyConversions[key] : key;
 
-export function validateKey(e:KeyboardEvent): Key|null  {
+export function validateKey(e: KeyboardEvent): Key | null {
     const key = convertKey(e.key);
-    return  isValidKey(key) ? key : null;
+    return isValidKey(key) ? key : null;
 }
