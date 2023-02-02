203bcbe4587dcb92a86f8a8b6284b88a2071c4fe
BADAKAA <Bastissh@gmail.com>
2023-02-02 02:11:39 +0100

    fix indentation

--- src/game-logic.ts
+++ src/game-logic.ts
@@ -31,46 +31,45 @@ export function changeRoom() {
   
     
 export function endQuicktime(room:room) {
+  getGameObject("hallway1").locked = false;
+  getGameObject("hallway1").sound.splice(0, 1);
+  sfx.openUp.play();
+  G.setRoom(room);
+  G.setSelection(0);
+  setTimeout(() => sfx.quicktime.stop(), 8000);
+  G.clearHand();
+}
 
-    getGameObject("hallway1").locked = false;
-    getGameObject("hallway1").sound.splice(0, 1);
-    sfx.openUp.play();
-    G.setRoom(room);
-    G.setSelection(0);
-    setTimeout(() => sfx.quicktime.stop(), 8000);
-    G.clearHand();
+export function talkAI(command: string) {
+  if (command == "start") {
+    sfx.AIRestored.play();
+    G.clearHand()
+    G.currentRoom.options.splice(G.selectionNumber, 1);
+    getGameObject("hallway2").locked = false;
+    G.setConversation(true);
   }
-
- export function talkAI(command: string) {
-    if (command == "start") {
-      sfx.AIRestored.play();
-      G.clearHand()
-      G.currentRoom.options.splice(G.selectionNumber, 1);
-      getGameObject("hallway2").locked = false;
-      G.setConversation(true);
-    }
-    if (command == "yes" || command == "ArrowUp") {
-      sfx.yes.play();
-      G.setConversation(false);
-    }
-    if (command == "no" || command=="ArrowDown") {
-      sfx.no.play();
-      G.setConversation(false);
-    }
+  if (command == "yes" || command == "ArrowUp") {
+    sfx.yes.play();
+    G.setConversation(false);
   }
+  if (command == "no" || command=="ArrowDown") {
+    sfx.no.play();
+    G.setConversation(false);
+  }
+}
 
-  export function enter() {
-    const sel = G.selection;
-    if (!sel) return changeRoom();
+export function enter() {
+  const sel = G.selection;
+  if (!sel) return changeRoom();
 
-    if (sel.name === "lockedRoom" || sel.name === "lockedDoor") return sfx.locked.play();
+  if (sel.name === "lockedRoom" || sel.name === "lockedDoor") return sfx.locked.play();
 
-    if (G.currentRoom.name == "lab" && sel.name == "hallway1" && G.hand && G.hand.name == "crowbar") {
-      return changeRoom();
-    } 
-    if (sel.name == "lab") return getGameObject("lab").sound[1].play();
-    if (sel.name == "slit" && G.hand && G.hand.name == "disk") return talkAI("start");
-    if (sel.type == "item" || sel.type == "object") return sel.sound[1].play();
-    if (sel.name == "bridge") return gameOver("win");
-    changeRoom();
+  if (G.currentRoom.name == "lab" && sel.name == "hallway1" && G.hand && G.hand.name == "crowbar") {
+    return changeRoom();
+  } 
+  if (sel.name == "lab") return getGameObject("lab").sound[1].play();
+  if (sel.name == "slit" && G.hand && G.hand.name == "disk") return talkAI("start");
+  if (sel.type == "item" || sel.type == "object") return sel.sound[1].play();
+  if (sel.name == "bridge") return gameOver("win");
+  changeRoom();
 }
