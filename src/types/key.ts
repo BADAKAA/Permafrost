2133ed0b7c33e3878137c8e6ca2ef2a04dc70e6d
BADAKAA <Bastissh@gmail.com>
2023-02-09 12:07:44 +0100

    use Map instead of object

--- src/types/key.ts
+++ src/types/key.ts
@@ -1,18 +1,18 @@
-const keyConversions: { [key: string]: Key } = {
-    w: "ArrowUp",
-    s: "ArrowDown",
-    a: "ArrowLeft",
-    d: "ArrowRight",
-    " ": "Space",
-    NumpadEnter: "Enter",
-}
+const keyConversions: Map<string, Key> = new Map([
+    ['w', 'ArrowUp'],
+    ['s', 'ArrowDown'],
+    ['a', 'ArrowLeft'],
+    ['d', 'ArrowRight'],
+    [' ', 'Space'],
+    ['NumpadEnter', 'Enter'],
+])
 
-const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Enter", "Space", "Escape"] as const;
+const keys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter', 'Space', 'Escape'] as const;
 export type Key = typeof keys[number];
 
 const isValidKey = (key: string): key is Key => !!keys.find(k=>k==key);
 
-const convertKey = (key: string) => key in keyConversions ? keyConversions[key] : key;
+const convertKey = (key: string) => keyConversions.get(key) ?? key;
 
 export function validateKey(e: KeyboardEvent): Key | null {
     const key = convertKey(e.key);
